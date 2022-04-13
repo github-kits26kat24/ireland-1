186f37ce86f5070deba322632d75c482003d6369
Kits26kat24 <kay.tijani@gmail.com>
2022-04-13 16:30:07 +0100

    Dev still in progress

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@
 # https://registry.terraform.io/providers/hashicorp/aws/3.73.0/docs/resources/instance
 
 resource "aws_instance" "web_A" {
-  ami           = "ami-048be0ea83345e4c1"
+  ami           = "ami-00902223dd91e78c5"
   instance_type = "t3.micro"
   key_name      = "kits26kat2486"
   subnet_id     = aws_subnet.public_subnet_1.id
@@ -21,7 +21,7 @@ EOF
 }
 
 resource "aws_instance" "Web_B" {
-  ami           = "ami-048be0ea83345e4c1"
+  ami           = "ami-00902223dd91e78c5"
   instance_type = "t3.micro"
   key_name      = "kits26kat2486"
   subnet_id     = aws_subnet.public_subnet_2.id
@@ -39,7 +39,7 @@ EOF
 }
 
 resource "aws_instance" "Tomcat-App-Server-1" {
-  ami           = "ami-048be0ea83345e4c1"
+  ami           = "ami-00902223dd91e78c5"
   instance_type = "t3.micro"
   key_name      = "kits26kat2486"
   subnet_id     = aws_subnet.private_subnet_3.id
@@ -65,7 +65,7 @@ EOF
 }
 
 resource "aws_instance" "Tomcat-App-Server-2" {
-  ami           = "ami-048be0ea83345e4c1"
+  ami           = "ami-00902223dd91e78c5"
   instance_type = "t3.micro"
   key_name      = "kits26kat2486"
   subnet_id     = aws_subnet.private_subnet_4.id
