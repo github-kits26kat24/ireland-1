241e717fbbbf38027e88bd8bd28f563bfa1f5f1d
kits26kat24 <kay.tijani@gmail.com>
2022-04-23 07:43:19 +0100

    working on dev backend config repo

--- instance.tf
+++ instance.tf
@@ -3,89 +3,44 @@
 
 resource "aws_instance" "web_A" {
   ami           = "ami-00902223dd91e78c5"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   key_name      = "kits26kat2486"
   subnet_id     = aws_subnet.public_subnet_1.id
-  user_data     = <<EOF
-#!/bin/bash
-sudo yum update -y
-sudo yum install httpd -y
-sudo systemctl start httpd
-EOF
-
   tags = {
     Name        = "Dev-Web-Server-A"
-    Environment = "Technology"
-    Department  = "IT"
+    Environment = "Dev"
   }
 }
 
 resource "aws_instance" "Web_B" {
   ami           = "ami-00902223dd91e78c5"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   key_name      = "kits26kat2486"
   subnet_id     = aws_subnet.public_subnet_2.id
-  user_data     = <<EOF
-#!/bin/bash
-sudo yum update -y
-sudo yum install python-3 -y
-EOF
-
   tags = {
     Name        = "Dev-Web-Server-B"
-    Environment = "Health"
-    Department  = "NHS"
+    Environment = "Dev"
   }
 }
 
 resource "aws_instance" "Tomcat-App-Server-1" {
   ami           = "ami-00902223dd91e78c5"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   key_name      = "kits26kat2486"
   subnet_id     = aws_subnet.private_subnet_3.id
-  user_data     = <<EOF
-#!/bin/bash
-sudo apt-get update -y
-sudo apt-get install tomcat  -y
-sudo apt-get tomcat-webapps.noarch -y
-sudo apt-get tomcat-admin-webapps.noarch -y
-sudo apt-get install tomcat7-docs -y
-# Restart
-sudo /etc/init.d/tomcat7 restart -y
-
-# Stop
-sudo /etc/init.d/tomcat7 stop -y
-EOF
-
   tags = {
     Name        = "Dev-Tomcat-Server-A"
-    Environment = "Goverment"
-    Department  = "Agriculture"
+    Environment = "Dev"
   }
 }
 
 resource "aws_instance" "Tomcat-App-Server-2" {
   ami           = "ami-00902223dd91e78c5"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   key_name      = "kits26kat2486"
   subnet_id     = aws_subnet.private_subnet_4.id
-  user_data     = <<EOF
-#!/bin/bash
-sudo apt-get update -y
-sudo apt-get install tomcat  -y
-sudo apt-get tomcat-webapps.noarch -y
-sudo apt-get tomcat-admin-webapps.noarch -y
-sudo apt-get install tomcat7-docs -y
-# Restart
-sudo /etc/init.d/tomcat7 restart -y
-
-# Stop
-sudo /etc/init.d/tomcat7 stop -y
-EOF
-
   tags = {
     Name        = "Dev-Tomcat-Server-B"
-    Environment = "Authomobile"
-    Department  = "Hydrid"
+    Environment = "Dev"
   }
 }
