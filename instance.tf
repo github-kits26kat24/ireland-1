4350d452dc05c589b81bff64a12c12bbfc751d6d
kits26kat24 <kay.tijani@gmail.com>
2022-04-28 05:02:24 +0100

    Prod repo

--- instance.tf
+++ instance.tf
@@ -6,17 +6,10 @@ resource "aws_instance" "web_A" {
   instance_type = "t3.micro"
   key_name      = "kits26kat2486"
   subnet_id     = aws_subnet.public_subnet_1.id
-  user_data     = <<EOF
-#!/bin/bash
-sudo yum update -y
-sudo yum install httpd -y
-sudo systemctl start httpd
-EOF
 
   tags = {
-    Name        = "Prod-Web-Server-A"
-    Environment = "Technology"
-    Department  = "IT"
+    Name        = "Web-Server-A"
+    Environment = "Prod"
   }
 }
 
@@ -25,16 +18,10 @@ resource "aws_instance" "Web_B" {
   instance_type = "t3.micro"
   key_name      = "kits26kat2486"
   subnet_id     = aws_subnet.public_subnet_2.id
-  user_data     = <<EOF
-#!/bin/bash
-sudo yum update -y
-sudo yum install python-3 -y
-EOF
-
+ 
   tags = {
-    Name        = "Prod-Web-Server-B"
-    Environment = "Health"
-    Department  = "NHS"
+    Name        = "Web-Server-B"
+    Environment = "Prod"
   }
 }
 
@@ -43,24 +30,10 @@ resource "aws_instance" "Tomcat-App-Server-1" {
   instance_type = "t3.micro"
   key_name      = "kits26kat2486"
   subnet_id     = aws_subnet.private_subnet_3.id
-  user_data     = <<EOF
-#!/bin/bash
-sudo apt-get update -y
-sudo apt-get install tomcat  -y
-sudo apt-get tomcat-webapps.noarch -y
-sudo apt-get tomcat-admin-webapps.noarch -y
-sudo apt-get install tomcat7-docs -y
-# Restart
-sudo /etc/init.d/tomcat7 restart -y
-
-# Stop
-sudo /etc/init.d/tomcat7 stop -y
-EOF
-
+ 
   tags = {
-    Name        = "Dev-Tomcat-Server-A"
-    Environment = "Goverment"
-    Department  = "Agriculture"
+    Name        = "Tomcat-Server-A"
+    Environment = "Prod"
   }
 }
 
@@ -69,23 +42,9 @@ resource "aws_instance" "Tomcat-App-Server-2" {
   instance_type = "t3.micro"
   key_name      = "kits26kat2486"
   subnet_id     = aws_subnet.private_subnet_4.id
-  user_data     = <<EOF
-#!/bin/bash
-sudo apt-get update -y
-sudo apt-get install tomcat  -y
-sudo apt-get tomcat-webapps.noarch -y
-sudo apt-get tomcat-admin-webapps.noarch -y
-sudo apt-get install tomcat7-docs -y
-# Restart
-sudo /etc/init.d/tomcat7 restart -y
-
-# Stop
-sudo /etc/init.d/tomcat7 stop -y
-EOF
 
   tags = {
-    Name        = "Prod-Tomcat-Server-B"
-    Environment = "Authomobile"
-    Department  = "Hydrid"
+    Name        = "Tomcat-Server-B"
+    Environment = "Prod"
   }
 }
