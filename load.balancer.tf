27b192ea2696a6acdc10ac7053b5d422850387c9
kits26kat24 <kay.tijani@gmail.com>
2022-04-30 03:16:42 +0100

    wip repo

--- load.balancer.tf
+++ load.balancer.tf
@@ -1,6 +1,6 @@
 
 resource "aws_lb" "project03_lb" {
-  name               = "Assignment03-LB"
+  name               = "Project03-LB"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.Project03.id]
@@ -31,7 +31,7 @@ resource "aws_lb_listener" "front_end" {
 }
 
 resource "aws_lb_target_group" "project03" {
-  name     = "Assignment03-lb-tg"
+  name     = "Projectt03-lb-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.project03.id
